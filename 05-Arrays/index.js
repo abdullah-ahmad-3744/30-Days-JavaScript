b90fe833d0d3c39f1b202eaca52420c6dca0c413
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-01 17:43:29 +0500

    Day 5 of javaScript practicing Arrays and array methods with some practice questions

--- 05-Arrays/index.js
+++ 05-Arrays/index.js
@@ -3,7 +3,7 @@ const prompt = require("prompt-sync") ();
 // Create an array and print it's all elements
 let array = [1,2,3,4,5,6,7,8,9,10];
 for (let index = 0; index < array.length; index++) {
-    // console.log(array[index]);    
+    console.log(array[index]);    
 }
 // *********************************************************************************************************************
 
@@ -15,7 +15,7 @@ for (let index = 0; index < array.length; index++) {
 let months = ['january', 'july', 'march', 'august'];
 months.shift();
 months.splice(1, 0, "june")
-// console.log(months);
+console.log(months);
 // *********************************************************************************************************************
 
 
@@ -25,23 +25,52 @@ months.splice(1, 0, "june")
  let numberArray1 = [1,2,3,4,5];
  let numberArray2 = [6,7,8,9,10]
  let numberArray3 = numberArray1.concat(numberArray2);
-//  console.log(numberArray3);
+ console.log(numberArray3);
 
  let primaryColors = ["red", "yellow", "blue"]
  let secondaryColors = ["orange", "green", "voilet"]
  let allColors = primaryColors.concat(secondaryColors)
-//  console.log(allColors);
+ console.log(allColors);
 //  Reversing the arrays 
 let reversedNumberArray = numberArray3.reverse()
-// console.log(reversedNumberArray);
+console.log(reversedNumberArray);
 let reversedPrimaryColors = primaryColors.reverse()
-// console.log(reversedPrimaryColors);
+console.log(reversedPrimaryColors);
 
 
 
 
-// ************************************   Q No.2    *********************************************************
+// ************************************   Q No.3    *********************************************************
 // Return the index of the javaScript from the given array if it was reversed
 let programmingLanguages = ['c', 'c++', 'html','javaScript', 'python', 'java', 'c#','sql'];
-programmingLanguages.reverse(); // Reversing the arry 
-console.log(programmingLanguages.indexOf("javaScript")); // Index of javaScript will be 4 after reversing the array
+console.log(programmingLanguages.reverse().indexOf("javaScript"));// Index of javaScript will be 4 after reversing array
+// *********************************************************************************************************************
+
+
+
+
+// ************************************   Q No.4    *********************************************************
+// Qs1. Write a JavaScript program to get the first n positive elements of an array. For example: for array [7, 9, 0, -2] and n=3Print, [7, 9, 0]
+let positiveArray = [3,4,6,-5,30,-11,13]
+let newPositiveArray = [];
+for (let index = 0; index < array.length; index++) {
+    if (positiveArray[index] > 0) {
+        newPositiveArray.push(positiveArray[index])
+    } else {
+    }
+}
+console.log(newPositiveArray);
+//  ********************************************************************************************************************
+
+
+
+
+// ************************************   Q No.5    *********************************************************
+// You are given an integer array array The task is to find the sum of it.
+let intergerArray = [1,2,3,4,5]
+let integerArraySum = 0;
+for (let index = 0; index < intergerArray.length; index++) {
+    integerArraySum = integerArraySum + intergerArray[index]
+}
+console.log(integerArraySum);
+// *********************************************************************************************************************
