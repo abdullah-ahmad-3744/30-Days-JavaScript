f61f71d0adbe1a9eaa37f173e1214f29db10bc51
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-01 17:56:06 +0500

    Added the concept of 2D arrays in Question 4

--- 02-Input -And-Conditionals/practice.js
+++ 02-Input -And-Conditionals/practice.js
@@ -7,12 +7,12 @@ const prompt =  require("prompt-sync") ();
 // ************************************   Q No.1    *********************************************************
 // Create a number variable n and take input from user and check if the number is divisible ny 10 print "Good Number" , if not print "Normal number"
 
-// const number = prompt("Enter your number to chech whether it is divisible by 10 or not :- ")
-// if (number %10 == 0) {
-//     console.log(`${number} is a Good Number `);
-// } else {
-//     console.log(`${number} is a Normal Number`);
-// }
+const number = prompt("Enter your number to chech whether it is divisible by 10 or not :- ")
+if (number %10 == 0) {
+    console.log(`${number} is a Good Number `);
+} else {
+    console.log(`${number} is a Normal Number`);
+}
 // *******************************************************************************************************************
 
 
@@ -20,14 +20,14 @@ const prompt =  require("prompt-sync") ();
 
 // ************************************   Q No.2    *********************************************************
 // Take a number as an input from the user and check it is positive or not 
-// const positiveCheck = Number( prompt("Enter your number to check is it positive or not :- ") );
-// if (positiveCheck > 0) {
-//     console.log(`${positiveCheck} is a positive number`);
-// } else if (positiveCheck == 0) {
-//     console.log(`Your number is zero`);
-// } else {
-//     console.log(`${positiveCheck} is a negative number`);
-// }
+const positiveCheck = Number( prompt("Enter your number to check is it positive or not :- ") );
+if (positiveCheck > 0) {
+    console.log(`${positiveCheck} is a positive number`);
+} else if (positiveCheck == 0) {
+    console.log(`Your number is zero`);
+} else {
+    console.log(`${positiveCheck} is a negative number`);
+}
 // ********************************************************************************************************************
 
 
@@ -35,12 +35,12 @@ const prompt =  require("prompt-sync") ();
 
 // ************************************   Q No.3    *********************************************************
 // A string is a golden string if it is started with "A" or "a" And has a total length greater than 5 for a given string check and print is it a golden string or not 
-// const goldenSringCheck = prompt("Enter your string to check is is a golden string or not :- ");
-// if (goldenSringCheck[0] === "A" || goldenSring[0] === "a" && goldenSring.length > 5) {
-//     console.log(`${goldenSringCheck} is a golden string`);
-// } else {
-//     console.log(`${goldenSringCheck} is a normal string`);
-// }
+const goldenSringCheck = prompt("Enter your string to check is is a golden string or not :- ");
+if (goldenSringCheck[0] === "A" || goldenSringCheck[0] === "a" && goldenSringCheck.length > 5) {
+    console.log(`${goldenSringCheck} is a golden string`);
+} else {
+    console.log(`${goldenSringCheck} is a normal string`);
+}
 // *********************************************************************************************************************
 
 
@@ -48,13 +48,12 @@ const prompt =  require("prompt-sync") ();
 
 // ************************************   Q No.4    *********************************************************
 // Take a number from user and check is it odd or even
-
-// const usernumber = Number(prompt("Enter your number to check OOD or Even :- "));
-// if (usernumber % 2 == 0) {
-//     console.log(`${usernumber} is an Even Number`);
-// } else {
-//     console.log(`${usernumber} is an ODD number`);
-// }
+const usernumber = Number(prompt("Enter your number to check OOD or Even :- "));
+if (usernumber % 2 == 0) {
+    console.log(`${usernumber} is an Even Number`);
+} else {
+    console.log(`${usernumber} is an ODD number`);
+}
 // *********************************************************************************************************************
 
 
@@ -80,23 +79,21 @@ if (userAge >= 18) {
 // Month in third Quarter [July, August, September]
 // Months in fourth Quarter [October, November, December]
 
-let firstQuarter = ["January","February", "March"] ;
-let secondQuarter = ["April","May", "June"] ;
-let thirdQuarter = [ "July", "August", "September"];
-let fourthQuarter = [ "October", "November", "December"];
+let monthsInYear = [['January', 'February', 'March'], ['April', "May", 'June'], ['July', 'August', 'September'], ['October', 'November', 'December']] ;
+
 const quarterNumber = Number( prompt("Enter Number from 1 to 4 to print the months of quarter :- "));
 switch (quarterNumber) {
     case 1:
-        console.log(firstQuarter);
+        console.log(monthsInYear[0]);
         break;
     case 2 : 
-        console.log(secondQuarter);
+        console.log([monthsInYear[1]]);
         break;
     case 3 : 
-        console.log(thirdQuarter);
+        console.log(monthsInYear[2]);
         break;
     case 4 : 
-        console.log(fourthQuarter);
+        console.log(monthsInYear[3]);
         break
     default:
         break;
