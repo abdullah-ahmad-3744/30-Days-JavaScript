2968e5d575452d47b35c11d2d2433dd9d7808dec
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-01 14:50:18 +0500

    Day 5 of javaScript practicing Arrays and array methods, concatination of arrays

--- 05-Arrays/index.js
+++ 05-Arrays/index.js
@@ -1,10 +1,10 @@
 const prompt = require("prompt-sync") ();
 // ************************************   Q No.1    *********************************************************
 // Create an array and print it's all elements
-// let array = [1,2,3,4,5,6,7,8,9,10];
-// for (let index = 0; index < array.length; index++) {
-//     console.log(array[index]);    
-// }
+let array = [1,2,3,4,5,6,7,8,9,10];
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);    
+}
 // *********************************************************************************************************************
 
 
@@ -12,10 +12,10 @@ const prompt = require("prompt-sync") ();
 
 // ************************************   Q No.2    *********************************************************
 // Change the given array ['january', 'july', 'march', 'august'] to this ['july', 'june', 'march', 'august'];
-// let months = ['january', 'july', 'march', 'august'];
-// months.shift();
-// months.splice(1, 0, "june")
-// console.log(months);
+let months = ['january', 'july', 'march', 'august'];
+months.shift();
+months.splice(1, 0, "june")
+console.log(months);
 // *********************************************************************************************************************
 
 
