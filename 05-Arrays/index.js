88056a39b6c950b90ffc2ddf69431586a5700260
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-01 14:11:29 +0500

    Day 5 of javaScript practicing Arrays and array methods

---
+++ 05-Arrays/index.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync") ();
+// ************************************   Q No.1    *********************************************************
+// Create an array and print it's all elements
+let array = [1,2,3,4,5,6,7,8,9,10];
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);    
+}
+// *********************************************************************************************************************
