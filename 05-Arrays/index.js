7efc6317e4cf8092f2611214c4916ae2ea5f91ce
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-01 14:27:44 +0500

    Day 5 of javaScript practicing Arrays and array methods practicee question 2

--- 05-Arrays/index.js
+++ 05-Arrays/index.js
@@ -6,3 +6,14 @@ for (let index = 0; index < array.length; index++) {
     console.log(array[index]);    
 }
 // *********************************************************************************************************************
+
+
+
+
+// ************************************   Q No.2    *********************************************************
+// Change the given array ['january', 'july', 'march', 'august'] to this ['july', 'june', 'march', 'august'];
+let months = ['january', 'july', 'march', 'august'];
+months.shift();
+months.splice(1, 0, "june")
+console.log(months);
+// *********************************************************************************************************************
