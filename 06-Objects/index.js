cb6add2a12bd6b29504981d943e9191fc9d16f5c
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-02 16:31:58 +0500

    Day 6 of javaScript, Practicing Object literals

---
+++ 06-Objects/index.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync");
+
+// Creating an object in javaScript 
+const Object = {
+    name : "Abdulah Aahmad",
+    age : 23,
+    address : "Bahawalpur"
+}
+// Accessin the object properties 
+console.log(Object.name);
+console.log(Object.address);
+// *********************************************************************************************************************
+
+
+
+
+// ************************************   Q No.4    *********************************************************
+// Creat an object literal for the properties of thread/twitter post which includes =>[ userName, content, likes, reposts,tags ]
+
+const post = {
+    userName : "@JohnSmith",
+    content : "This is my #firstpost",
+    likes : 300,
+    reposts : 21,
+    tags : ["@adam", "@Ali"]
+}
+console.log(post.likes);
