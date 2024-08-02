5ae2493284d32b8410166a12498138dbfce5995c
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-02 15:34:57 +0500

    Practicing loops , Added two mre Questions in loops practice

--- 03-loops/index.js
+++ 03-loops/index.js
@@ -1,9 +1,9 @@
 const prompt = require ("prompt-sync") ();
 // ************************************   Q No.1    *********************************************************
 // Print the numbers from 1 to 10.
-// for (let index = 1; index <=10; index++) {
-//     console.log(index);
-// }
+for (let index = 1; index <=10; index++) {
+    console.log(index);
+}
 // ********************************************************************************************************************
 
 
@@ -11,34 +11,34 @@ const prompt = require ("prompt-sync") ();
 
 // ************************************   Q No.2    *********************************************************
 // Write a program to get the number from user and print it's table. 
-// const userInput = Number(prompt("Enter your number to print it's table :- "));
-// for (let index = 1; index <= 10; index++) {
-//     console.log(` ${userInput } * ${index} = ${userInput * index}`);
-// }
+const userInput = Number(prompt("Enter your number to print it's table :- "));
+for (let index = 1; index <= 10; index++) {
+    console.log(` ${userInput } * ${index} = ${userInput * index}`);
+}
 // *********************************************************************************************************************
 
 
 
 // ************************************   Q No.3    *********************************************************
 // Print the sum of n natural numbers from 1 to n . Get the number as input from the user 
-// const userNumber = Number (prompt ("Enter your number to print the sum of numbers from 1 to n :- "));
-// let sum =0;
-// for (let index = 1; index <= userNumber; index++) {
-//     sum = sum + index;
-// }
-// console.log(sum);
+const userNumber = Number (prompt ("Enter your number to print the sum of numbers from 1 to n :- "));
+let sum =0;
+for (let index = 1; index <= userNumber; index++) {
+    sum = sum + index;
+}
+console.log(sum);
 // *********************************************************************************************************************
 
 
 
 // ************************************   Q No.4    *********************************************************
 // Write a program to find the factorial of a number.
-// let result = 1;
-// let number = Number(prompt ("Enter your number to print it's factorial :- "));
-// for (let index = 1; index <= number; index++) {
-//     result = index * result;
-// }
-// console.log(`Factorial of ${number} = ${result}`);
+let result = 1;
+let number = Number(prompt ("Enter your number to print it's factorial :- "));
+for (let index = 1; index <= number; index++) {
+    result = index * result;
+}
+console.log(`Factorial of ${number} = ${result}`);
 // *********************************************************************************************************************
 
 
@@ -46,8 +46,8 @@ const prompt = require ("prompt-sync") ();
 
 // ************************************   Q No.5    *********************************************************
 // Print All ODD numbers seperately from 1 to n 
-let userInput = Number(prompt("Enter your Number :- "));
-for (let index = 1; index <= userInput; index++) {
+let userInput2 = Number(prompt("Enter your Number :- "));
+for (let index = 1; index <= userInput2; index++) {
     if (index % 2 !== 0) {
         console.log(`${index} = is an ODD Number`);
         
@@ -62,8 +62,8 @@ for (let index = 1; index <= userInput; index++) {
 
 // ************************************   Q No.6    *********************************************************
 // Print All Even numbers seperately from 1 to n 
-let userInput2 = Number(prompt("Enter your Number :- "));
-for (let index = 1; index <= userInput2; index++) {
+let userInput3 = Number(prompt("Enter your Number :- "));
+for (let index = 1; index <= userInput3; index++) {
     if (index % 2 == 0) {
         console.log(`${index} = is an Even Number`);
         
