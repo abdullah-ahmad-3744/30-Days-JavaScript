4e53bff1a4ecaaf149436c8327b27db9cdb1263e
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-03 14:23:33 +0500

    Practicing Math Object

--- 06-Objects/index.js
+++ 06-Objects/index.js
@@ -60,7 +60,7 @@ console.log(student); // Marks property is deleted from student object
 // ************************************   Q No.3    *********************************************************
 const classRoom = {
     student1 : 
-     {name: "Juli",city: "New York",gender: "Female",marks: {  math: 85,  science: 90,  english: 88,history: 76 }},
+    {name: "Juli",city: "New York",gender: "Female",marks: {  math: 85,  science: 90,  english: 88,history: 76 }},
     student2 :  
     {name: "Bob",city: "Los Angeles",gender: "Male",marks: {  math: 78,  science: 82,  english: 80,  history: 74}},
     student3 :  
@@ -68,7 +68,7 @@ const classRoom = {
     student4 :  
     {name: "Diana",city: "Houston",gender: "Female",marks: {  math: 90,  science: 92,  english: 91,  history: 89}},
     student5 :
-     {name: "John",city: "Phoenix",gender: "Male",marks: {math: 75,science: 80,english: 78,history: 72}}};
+    {name: "John",city: "Phoenix",gender: "Male",marks: {math: 75,science: 80,english: 78,history: 72}}};
   console.log(classRoom); // Accessing all students of class 
 
 
@@ -88,4 +88,34 @@ console.log(classRoom.student5.marks.math = 85);  // Marks of John updated from 
 //  Calculate the average marks for July:
 let julyMarks = classRoom.student1.marks
 console.log(julyAverageMarks = (julyMarks.math + julyMarks.science + julyMarks.english + julyMarks.history)/4);// Average of july marks are 84.75
+// ************************************************************************************************************************
+
+
+
+
+// Math object in  and it's most commonly used methds js 
+let absoluteMethod = Math.abs(12); // Absolute method of Math Object 
+console.log(absoluteMethod);
+console.log(Math.abs(-23));
+
+
+// Random metod of math object 
+let randomNumber = Math.random(); //Generates a random numner
+console.log(randomNumber);
+
+
+// Floor method of Math object 
+let floorMethod = Math.floor(5.67892); // Round off to its nearest bottom number 
+console.log(floorMethod);
+
+
+// Ceil method of Math object
+let ceilMethod = Math.ceil(8.0000122340); // Round of to it's nearest upper/top number 
+console.log(ceilMethod);
+
+
+// Power Method of Math Object 
+let powerMethod = Math.pow(2,8); 
+console.log(powerMethod);
+
 // *********************************************************************************************************************
