fbf07b3922579ac580d32f9dc7c9bbc381618155
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-05 16:01:43 +0500

    Day 7 of javaScript , Practicing basic concenpts Question No.2

--- 07-Practice/index.js
+++ 07-Practice/index.js
@@ -1,4 +1,6 @@
 const prompt = require ("prompt-sync") ();
+// ************************************   Q No.1    *********************************************************
+// User will guess my favorite book 
 console.log(" ");
 console.log(`****************************************************************`);
 console.log(" ");
@@ -20,4 +22,30 @@ if ( userInput == myFavoriteBook) {
     console.log("You quited the game :- ");
     
 }
+// *********************************************************************************************************************
 
+
+
+
+// ************************************   Q No.2    *********************************************************
+// User enters a maximum number and then tries to guess the random generated number between 1 to maximum number/user number 
+console.log("********************** Enter 0 to quit the game **********************");
+
+let userNumber = parseInt(prompt("Enter a number :- "))
+let randomNumber = Math.floor(Math.random() * userNumber) + 1;
+// console.log(randomNumber);
+let userGuess = parseInt(prompt("Guess the random generated number :- "))
+while(true) {
+    if (userGuess == 0) {
+        console.log("You quit the game :- ");
+        break;
+        
+    } else if (userGuess == randomNumber) {
+        console.log("Congrats :- ");
+        break;
+    } else {
+        userGuess = parseInt(prompt ("Wrong guess. please try again :- "))
+        
+    }
+}
+// *********************************************************************************************************************
