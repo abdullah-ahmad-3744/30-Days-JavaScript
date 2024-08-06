9315e180223b1013d6ed888dc5e6afd8d6b6d50a
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-06 13:13:46 +0500

    Practicing functions

--- 08-Functions/index.js
+++ 08-Functions/index.js
@@ -6,3 +6,25 @@ function hello () {
 
 // Calling the function
 hello()
+// ********************************************************************************************************************
+
+
+
+
+// Write a function which prints the numbers from 1 to 10 
+function printNumbers () {
+    for (let index = 1; index <= 10; index++) {
+        console.log(index);
+        
+    }
+}
+// printNumbers();
+
+
+// Create a function to roll a dice and always display the value of the dice (1 to 6)
+
+function rollDice() {
+    let randomNumber = Math.floor(Math.random() * 6) + 1 ;
+    console.log(randomNumber);
+}
+rollDice();
