02242bba75a6e3362dec71568d95a277c82bc600
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-08-13 19:08:04 +0500

    Practicing DOM manipultion with background color changer

--- 11-DOM/app.js
+++ 11-DOM/app.js
@@ -1,3 +1,25 @@
 let container = document.querySelector(".container");
 let colorBox = document.querySelector(".color-box");
 let button = document.querySelectorAll(".button");
+button.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        if (event.target.id === "yellow") {
+            container.style.backgroundColor = event.target.id
+        } else if (event.target.id === "green") {
+            container.style.backgroundColor = event.target.id
+        } else if (event.target.id === "orange") {
+            container.style.backgroundColor = event.target.id
+        } else if (event.target.id === "black") {
+            container.style.backgroundColor = event.target.id
+        } else if (event.target.id === "red") {
+            container.style.backgroundColor = event.target.id
+        } else if (event.target.id === "blue") {
+            container.style.backgroundColor = event.target.id
+        } else if (event.target.id === "white") {
+            container.style.backgroundColor = event.target.id
+        } else {
+            console.log("Nothing");
+            
+        }
+    })
+})
