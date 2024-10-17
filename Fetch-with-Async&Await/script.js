f33315e0811e85271efe6fb597c63f365f670065
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-10-17 11:15:23 +0500

    Using try catch block to catch the errors

--- Fetch-with-Async&Await/script.js
+++ Fetch-with-Async&Await/script.js
@@ -1,26 +1,30 @@
-let apiUrl = "https://catfact.ninja/fact";
+let apiUrl = "https://catfact.ninja/fact2";
 
 async function fetchData() {
+  try {
     // The await keywords wait for the response from the server as it is asynchronous so we have to wait for the response from the server befor using it
-    let response = await fetch(apiUrl);  
+    let response = await fetch(apiUrl);
     let validResponse = await response.json();
     console.log(response);
     console.log(validResponse);
-    console.log("======================================================================");
-    
+    console.log(
+      "======================================================================"
+    );
+
     // sending anothe request to the server
     let response2 = await fetch(apiUrl);
     console.log(response2);
-    
     let validResponse2 = await response2.json();
     console.log(validResponse2);
     // Accessing the data from the response
     console.log(validResponse2.fact);
-    
-    
-    
-    
-    
-};
+  } catch (error) {
+    console.log("Error is = ",error);
 
-fetchData()
+    console.log("Bye");
+    
+    
+  }
+}
+
+fetchData();
