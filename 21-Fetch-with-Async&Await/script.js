3349a36a0d33e3a835db03f537c69ede1e7ebcbf
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-10-18 10:07:35 +0500

    A little bit styling added to the Fetch-with-Async and Await , And new directory added of day22 of 30 days of javaScript , Using Axios instead of fetch method .

--- 21-Fetch-with-Async&Await/script.js
+++ 21-Fetch-with-Async&Await/script.js
@@ -1,30 +1,21 @@
-let apiUrl = "https://catfact.ninja/fact2";
+let apiUrl = "https://catfact.ninja/fact";
+let factButton = document.querySelector(".fact-button");
+let factBox = document.querySelector(".fact-box")
 
 async function fetchData() {
   try {
     // The await keywords wait for the response from the server as it is asynchronous so we have to wait for the response from the server befor using it
     let response = await fetch(apiUrl);
     let validResponse = await response.json();
-    console.log(response);
-    console.log(validResponse);
-    console.log(
-      "======================================================================"
-    );
-
-    // sending anothe request to the server
-    let response2 = await fetch(apiUrl);
-    console.log(response2);
-    let validResponse2 = await response2.json();
-    console.log(validResponse2);
-    // Accessing the data from the response
-    console.log(validResponse2.fact);
+    // console.log(response);
+    // console.log(validResponse.fact);
+    factBox.textContent = validResponse.fact
   } catch (error) {
-    console.log("Error is = ",error);
-
-    console.log("Bye");
-    
-    
+    console.log("Error is = ",error);    
   }
 }
 
-fetchData();
+factButton.addEventListener("click", () => {
+  // factBox.textContent = " "
+  fetchData();
+})
